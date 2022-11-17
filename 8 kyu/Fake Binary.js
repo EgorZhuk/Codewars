c319f213f9aee6924a325e87f4599280066056a1
EgorZhuk <egorbug@gmail.com>
2022-11-17 17:40:43 +0300

    Add 8kyu kata "Fake Binary"

---
+++ 8 kyu/Fake Binary.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/57eae65a4321032ce000002d
+
+// 8 kyu
+// Fake Binary
+
+// DESCRIPTION:
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+function fakeBin(x) {
+  let y = x.split('');
+  for (let i=0; i<=y.length; i++){
+    if (y[i]>=5){
+      y[i]=1;
+    } else if (y[i]<5){
+      y[i]=0;
+    };
+  }
+  return y.join('');
+}
