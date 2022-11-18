33f27f5ee61a44ce6cf9092dc55b264f02aeedb6
EgorZhuk <egorbug@gmail.com>
2022-11-18 18:33:13 +0300

    Add 8kyu kata "Remove String Spaces"

---
+++ 8 kyu/Remove String Spaces.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+// 8 kyu
+// Remove String Spaces
+
+// DESCRIPTION:
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+  return x.split(' ').join('')
+}
