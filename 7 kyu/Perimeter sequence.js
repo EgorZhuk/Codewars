6f0a4e6c78cc88e6a2f3557cd11014f5a0787c9f
EgorZhuk <egorbug@gmail.com>
2022-11-20 11:22:12 +0300

    Add 7kyu kata "Perimeter sequence"

---
+++ 7 kyu/Perimeter sequence.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/589519d1f0902e01af000054
+
+// 7 kyu
+// Perimeter sequence
+
+// DESCRIPTION:
+// Perimeter sequence
+// The first three stages of a sequence are shown.
+
+// blocks
+
+// The blocksize is a by a and a ≥ 1.
+
+// What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+
+function perimeterSequence(a, n) {
+  return 4 * a * n;
+}
