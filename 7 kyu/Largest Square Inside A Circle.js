7b589f67d7359164119cc502800b74016048d13e
EgorZhuk <egorbug@gmail.com>
2022-11-20 10:23:12 +0300

    Add 7kyu kata "Largest Square Inside A Circle"

---
+++ 7 kyu/Largest Square Inside A Circle.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5887a6fe0cfe64850800161c
+
+// 7 kyu
+// Largest Square Inside A Circle
+
+// DESCRIPTION:
+// Determine the area of the largest square that can fit inside a circle with radius r.
+
+function areaLargestSquare(r) {
+  return 2 * r ** 2;
+}
